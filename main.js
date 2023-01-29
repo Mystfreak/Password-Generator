6e1df87f9087e91f61bcf642f585634b5c06e5ff
Khatra Ahmed <[khatra.ahmed@gmail.com]>
2023-01-29 19:36:40 +0000

    Changing prompt message.

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ var specialCharacters = [
   
   // Function to prompt user for password options
   function getPasswordOptions() {
-    let passLength = prompt("Type the length of password you want [At least 8 Characters but no more than 128]");
+    let passLength = prompt("Type password that is at least 8 Characters but no more than 128");
     let lowerCase = confirm("Do you want to include lowercase letters?");
     let upperCase = confirm("Do you want to include uppercase letters?");
     let numeric = confirm("Do you want to include numbers?");
